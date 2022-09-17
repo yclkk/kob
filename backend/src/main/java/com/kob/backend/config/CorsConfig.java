401c0392583be0172050848dadfad040eacb2238
yclkk <1024917167@qq.com>
2022-09-17 22:21:24 +0800

    配置环境，使用SpringSecurity

--- backend/src/main/java/com/kob/backend/config/CorsConfig.java
+++ backend/src/main/java/com/kob/backend/config/CorsConfig.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+/**
+ * 解决跨域配置
+ */
 @Configuration
 public class CorsConfig implements Filter {
     @Override
--- backend/src/test/java/com/kob/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/kob/backend/BackendApplicationTests.java
@@ -2,12 +2,24 @@ package com.kob.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.sql.SQLOutput;
+import java.util.logging.Logger;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		System.out.println(passwordEncoder.encode("zyc"));
+		System.out.println(passwordEncoder.encode("zyc"));
+		System.out.println(passwordEncoder.matches("zyc", "$2a$10$t/g4ReQX1PXS6R15qPlqTOmzK4DB1epTUKSrWzB7ezKgF3LtJUBkK"));
+		System.out.println(passwordEncoder.matches("zyc", "$2a$10$rP/xwjlFw62wRmCu26IKOO67jkz99Dzok7xaIonbsE5z3Z8Wrthwa"));
+
+
 	}
 
 }
